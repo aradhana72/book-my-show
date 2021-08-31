f40133a30e2bccfd93845bf5b71ce23554d3da1b
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2021-08-31 18:34:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route } from "react-router-dom";
+
+//HOC
+import DefaultHOC from "./HOC/Default.HOC";
+
+//Component
+import Temp from "./components/temp";
+
+function App() {
+  return (
+    <>
+     <DefaultHOC path="/" exact component={Temp} />
+    </>
+  );
+}
+// / , /movie
+export default App;
