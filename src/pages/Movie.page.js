7dd9c9f9f25db7304f6178573030d18d435080ca
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2021-09-07 19:48:11 +0530

    Add files via upload

---
+++ src/pages/Movie.page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const  Movie = () => {
+  return (
+    <>
+   <div>
+   <img src="https://in.bmscdn.com/promotions/cms/creatives/1629830049996_celebfiecruisewiththestarsrevised_webshowcase_1240x300_24aug.jpg" />
+   </div>
+    </>
+  );
+};
+
+export default Movie;
