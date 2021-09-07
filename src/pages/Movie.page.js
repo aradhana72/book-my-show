2921e323b1747e8097d0e89256f2f300eabfec70
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2021-09-07 19:21:01 +0530

    Add files via upload

---
+++ src/pages/Movie.page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const  Movie = () => {
+  return (
+    <>
+   <div>
+   <img src="https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/chehre-et00102880-25-08-2021-07-26-46.jpg" />
+   </div>
+    </>
+  );
+};
+
+export default Movie;
