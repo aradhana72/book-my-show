19a31c448d5e9ea2423d89bae8951b441359c081
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2021-09-10 19:05:53 +0530

    Add files via upload

---
+++ src/components/MovieHero/MovieHero.component.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MovieHero = () => {
+  return (
+    <>
+   <div className="md:hidden">
+    <img
+      src="https://in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/shang-chi-and-the-legend-of-the-ten-rings-et00122566-24-08-2021-02-01-36.jpg"
+      alt="poster"
+     />
+   </div>
+
+   <div className="hidden md:block lg:hidden"></div>
+
+   <div className="hidden lg:block"></div>
+    </>
+  )
+};
+
+export default MovieHero;
---
+++ src/pages/Movie.page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MovieHero from "../components/MovieHero/MovieHero.component";
+const  Movie = () => {
+  return (
+    <>
+    <MovieHero />
+    </>
+  );
+};
+
+export default Movie;
