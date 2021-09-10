4566c0b97cf27aa1ce95669e06c07e4f0daacbca
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2021-09-10 19:40:51 +0530

    Update MovieHero.component.js

--- src/components/MovieHero/MovieHero.component.js
+++ src/components/MovieHero/MovieHero.component.js
@@ -23,6 +23,14 @@ const MovieHero = () => {
 style={{ backgroundImage: "linear-gradient(90deg, rgb(34, 34, 34) 24.97%, rgb(34, 34, 34) 38.3%, rgba(34, 34, 34, 0.04) 97.47%, rgb(34, 34, 34) 100%)"}}
 />
 
+<div className="absolute z-30 w-64 h-96 left-64 top-10">
+<img
+src="https://in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/shang-chi-and-the-legend-of-the-ten-rings-et00122566-24-08-2021-02-01-36.jpg"
+alt="poster"
+className="h-full w-full rounded-xl"
+/>
+</div>
+
    <img
      src="https://in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/shang-chi-and-the-legend-of-the-ten-rings-et00122566-24-08-2021-02-01-36.jpg"
      alt="poster"
