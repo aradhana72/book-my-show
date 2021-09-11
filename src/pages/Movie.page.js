0711b61e8ac60eb3d7edaf4f3e5bc3cc5a67ede6
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2021-09-11 18:44:48 +0530

    Update Movie.page.js

--- src/pages/Movie.page.js
+++ src/pages/Movie.page.js
@@ -4,6 +4,17 @@ const  Movie = () => {
   return (
     <>
     <MovieHero />
+    <div className="my-12 container px-4 lg:w-1/2 lg:ml-64">
+      <div className="flex flex-col items-start gap-3">
+        <h2 className="text-gray-800 font-bold text-2xl">About the movie</h2>
+        <p>Shang-Chi and The Legend of The Ten Rings features Simu Liu as Shang-Chi, who must confront the past he thought he left behind when he is drawn into the web of the mysterious Ten Rings organization. The film is directed by Destin Daniel Cretton and produced by Kevin Feige and Jonathan Schwartz.</p>
+      </div>
+  <div className="my-8">
+  <hr />
+  </div>
+
+
+    </div>
     </>
   );
 };
