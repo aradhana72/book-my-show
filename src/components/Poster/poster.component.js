b188a396314154c888fe9179befe49414035ca88
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2021-09-17 19:20:07 +0530

    Update poster.component.js

--- src/components/Poster/poster.component.js
+++ src/components/Poster/poster.component.js
@@ -5,7 +5,8 @@ const Poster = (props) => {
     <>
     <div className="flex flex-col items-start gap-2 px-3">
       <div className="h-80" >
-       <img src={props.src} alt={props.title} className="w-full h-full rounded-xl"/>
+       <img src={`https://image.tmdb.org/t/p/original${props.poster_path}`}
+       alt={props.original_title} className="w-full h-full rounded-xl"/>
       </div>
       <h3
       className={`text-lg font-bold ${
