338dd774927daa8247bcd854b744cc29b43fb42b
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2021-09-17 19:00:53 +0530

    Update HeroCarousal.component.js

--- src/components/HeroCarousal/HeroCarousal.component.js
+++ src/components/HeroCarousal/HeroCarousal.component.js
@@ -16,7 +16,7 @@ const [images, setImages] = useState([]);
 useEffect(() => {
   const requestNowPlayingMovies = async() => {
     const getImages = await axios.get("/movie/now_playing");
-    console.log(getImages);
+    setImages(getImages.data.results);
   };
   requestNowPlayingMovies();
 }, []);
@@ -52,7 +52,8 @@ useEffect(() => {
   {
     images.map((image) => (
       <div className="w-full h-64 md:h-80 py-3">
-      <img src={image} alt="testing" className="w-full h-full rounded-md"/>
+      <img src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
+      alt="testing" className="w-full h-full rounded-md"/>
       </div>
     ))
   }
@@ -64,7 +65,8 @@ useEffect(() => {
   {
     images.map((image) => (
       <div className="w-full h-96 px-2 py-3">
-      <img src={image} alt="testing" className="w-full h-full rounded-md"/>
+      <img src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
+      alt="testing" className="w-full h-full rounded-md"/>
       </div>
     ))
   }
