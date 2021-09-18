8e764b6da2b179fa217f08c7b0d5422eebdba989
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2021-09-18 19:31:48 +0530

    Update Movie.page.js

--- src/pages/Movie.page.js
+++ src/pages/Movie.page.js
@@ -1,6 +1,24 @@
 import React from "react";
 import MovieHero from "../components/MovieHero/MovieHero.component";
 import {BiCameraMovie} from "react-icons/bi";
+
+const launchRazorPay = () => {
+  let options = {
+    key: "ur api id",
+    amount: 500*100,
+    currency: "INR",
+    name: "Book My Show Clone",
+    description: "Movie Purchase on Rental",
+    image: "https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png",
+    handler: () => {
+      alert("Payment Done")
+    },
+    theme: {color: "#c4242d"}
+  };
+  let rzp = new window.Razorpay(options);
+  rzp.open();
+};
+
 const  Movie = () => {
 return (
 <>
@@ -25,6 +43,11 @@ return (
          </div>
       </div>
    </div>
+
+   <button onClick={launchRazorPay} class="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-8 rounded">
+  Book tickets
+</button>
+
 </div>
 </>
 );
