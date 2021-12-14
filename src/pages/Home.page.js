295e503424a56842b3d5f7b413c34b369851c4ad
Aradhana Kund <73130056+aradhana72@users.noreply.github.com>
2021-12-14 16:34:51 +0530

    Update Home.page.js

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -23,27 +23,24 @@ console.log({popularMovies});
   return (
   <>
   <div className="flex flex-col gap-10">
-
-  <div className="container mx-auto px-4">
-  <h1 className="text-2xl font-bold text-gray-800">
-  The best of Entertainment
-  </h1>
-  <EntertainmentCardSlider />
-  </div>
-
-  <div className="bg-navCol-200 py-16">
-
-  <div className="container mx-auto px-4">
-  <div className="flex">
-   <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
-   alt="Rupay"
-   className="w-full h-full"
-    />
-  </div>
-   <PosterSlider images={popularMovies} title="Premiers" subtitle="Brand New Releases Every Friday" isDark/>
-  </div>
-  </div>
-  </div>
+   <div className="container mx-auto px-4">
+      <h1 className="text-2xl font-bold text-gray-800">
+         The best of Entertainment
+      </h1>
+      <EntertainmentCardSlider />
+   </div>
+   <div className="bg-navCol-200 py-16">
+      <div className="container mx-auto px-4">
+         <div className="flex">
+            <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+               alt="Rupay"
+               className="w-full h-full"
+               />
+         </div>
+         <PosterSlider images={popularMovies} title="Premiers" subtitle="Brand New Releases Every Friday" isDark/>
+      </div>
+   </div>
+</div>
 
   <div className="container mx-auto px-4">
   <PosterSlider
